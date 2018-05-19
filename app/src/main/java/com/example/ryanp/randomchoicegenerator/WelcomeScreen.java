c063a7e3c6d1dce3a2dd79e2685ecd07134b97d4
Ryan Pfeifer <ryanpfeifer1111@gmail.com>
2018-05-19 14:22:39 -0500

    welcome screen added

--- app/src/main/java/com/example/ryanp/randomchoicegenerator/WelcomeScreen.java
+++ app/src/main/java/com/example/ryanp/randomchoicegenerator/WelcomeScreen.java
@@ -1,13 +1,20 @@
 package com.example.ryanp.randomchoicegenerator;
 
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WelcomeScreen extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+        finish();
     }
+
 }
